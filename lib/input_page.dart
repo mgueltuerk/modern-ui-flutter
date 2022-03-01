b8c48c1228ca9fc46b8c4f4b0c074efa07630f8f
Murat <m.gueltuerk@gmx.de>
2022-03-01 10:52:46 +0100

    first commit

---
+++ lib/input_page.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  const InputPage({Key? key}) : super(key: key);
+
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MODERN UI'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Card(
+                    colour: Colors.blue.shade500,
+                  ),
+                ),
+                Expanded(
+                  child: Card(
+                    colour: Colors.purple.shade500,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Card(
+              colour: Colors.teal.shade500,
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Card(
+                    colour: Colors.amber.shade500,
+                  ),
+                ),
+                Expanded(
+                  child: Card(
+                    colour: Colors.indigoAccent.shade100,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: Colors.blueGrey.shade500,
+            margin: const EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: 80.0,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Card extends StatelessWidget {
+  const Card({Key? key, required this.colour}) : super(key: key);
+
+  final Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'input_page.dart';
+
+void main() => runApp( const BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+   const BMICalculator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: const Color(0xFF0A0E21),
+    colorScheme: const ColorScheme.light(primary: Color(0xFF0A0E21)),
+),
+      home:  const InputPage(),
+    );
+  }
+}
+
